03454bfed5424eaba5169d686823f09abbcea741
ywc <bl.cisse22@gmail.com>
2026-01-14 14:44:46 -0500

    AIA Routine Update,Read me

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   /* config options here */
+  experimental: {
+    turbo: {
+      resolveExtensions: ['.tsx', '.ts', '.jsx', '.js', '.mjs', '.json'],
+    },
+  },
+  // Exclude Python virtual environment from file watching
+  webpack: (config, { isServer }) => {
+    config.watchOptions = {
+      ...config.watchOptions,
+      ignored: ['**/1051-env/**', '**/__pycache__/**', '**/*.pyc'],
+    };
+    return config;
+  },
 };
 
 export default nextConfig;
